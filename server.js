a491e72ac496d63b1112218cd10bb49b98751c8a
benruby <benlevi1@gmail.com>
2016-06-20 00:25:01 +0300

    adding CORS - production

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var express = require('express');
+// var cors = require('cors')
 var app = express();
 
+// app.use(cors());
 app.set('port', (process.env.PORT || 9000));
 
 // app.use(express.static(__dirname + '/'));
