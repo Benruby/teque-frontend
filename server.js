ed182f52f844f8afb912259ad699967ac2aacd25
benruby <benlevi1@gmail.com>
2016-06-19 18:50:42 +0300

    changing server.js

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-var gzippo = require('gzippo');
-  var express = require('express');
-  var app = express();
- 
-  app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-  app.listen(process.env.PORT || 5000);
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 9000));
+
+//app.use(express.static(__dirname + '/'));
+//app.use(express.static(__dirname + '/app'));
+//app.use(express.static(__dirname + '/app/scripts'));
+app.use(express.static(__dirname + '/dist'));
+
+// views is directory for all template files
+//app.set('views', __dirname + '/app');
+//app.set('view engine', 'ejs');
+
+//app.get('/', function(request, response) {
+//  response.render('index');
+//});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
