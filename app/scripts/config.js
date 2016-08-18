fd7a5c0b0110d9b523088c259b203e8f4ca9256b
Benruby <benlevi1@gmail.com>
2016-08-18 12:14:58 +0300

    small fix to config.js

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,8 +5,8 @@ angular.module('config', [])
 .constant('ENV', {
   apiEndpoint: 'http://localhost:9000',
   env: 'development'
-  // apiEndpoint: 'https://teque-staging.herokuapp.com/',
+  // apiEndpoint: 'https://teque-staging.herokuapp.com',
   // env: 'staging'
-  // apiEndpoint: 'https://teque.herokuapp.com/',
+  // apiEndpoint: 'https://teque.herokuapp.com',
   // env: 'production'
 });
