626a8f7cb61850312df857c167602b6a0de69dd1
Benruby <benlevi1@gmail.com>
2016-08-18 17:29:05 +0300

    new relic

---
+++ app/newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['teque-frontend'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '07856d366b88ce42aca5294fed3a195f8d05eb07',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('newrelic');
 var express = require('express');
 // var cors = require('cors')
 var app = express();
